9495864dde6a8003266ca494237250f29a3f87fa
commander15 <128611219+commander15@users.noreply.github.com>
2025-10-24 12:59:37 +0100

    Enhanced workflow
    - Reworked workflow file
    - Added packaging script

---
+++ cmake/WorkflowSetup.cmake
@@ -0,0 +1,19 @@
+set(CPACK_PACKAGE_NAME        "Qt Workflow Test")
+set(CPACK_PACKAGE_VENDOR      "Commander Systems")
+set(CPACK_PACKAGE_DESCRIPTION "Awesome program.")
+set(CPACK_PACKAGE_DIRECTORY   ${CMAKE_BINARY_DIR}/releases)
+
+string(TIMESTAMP RELEASE_DATE "%y-%m-%d")
+
+include(CPackIFW)
+
+cpack_ifw_configure_component(Program
+    ESSENTIAL
+    NAME Program
+    DISPLAY_NAME "Program Core"
+    DESCRIPTION "Program core files."
+    VERSION ${PROJECT_VERSION}
+    RELEASE_DATE ${RELEASE_DATE}
+)
+
+include(CPack)
